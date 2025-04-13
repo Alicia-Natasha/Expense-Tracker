ff70090f34bf705691f09be62496e066ca9d661c
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-13 21:59:33 +0300

    expensetracker

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
