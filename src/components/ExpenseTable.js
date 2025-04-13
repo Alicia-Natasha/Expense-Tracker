e1704f76ce224a1e52a202935ad754e1bcdfa7d7
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-13 22:12:35 +0300

    ExpenseTable

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
-function ExpenseTable() {
+function ExpenseTable({ expenses }) {
   return (
-    <div>
-      
-    </div>
+    <table border="1" cellPadding="10" cellSpacing="0">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Amount ($)</th>
+      </tr>
+    </thead>
+    <tbody>
+      {expenses.length === 0 ? (
+        <tr>
+          <td colSpan="3" style={{ textAlign: 'center' }}>
+            No expenses found.
+          </td>
+        </tr>
+      ) : (
+        expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.name}</td>
+            <td>{expense.description}</td>
+            <td>{expense.amount.toFixed(2)}</td>
+          </tr>
+        ))
+      )}
+    </tbody>
+  </table>
   )
 }
 
