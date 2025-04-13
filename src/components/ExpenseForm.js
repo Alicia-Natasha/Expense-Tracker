37d33807b16a1b87c62636cdd28854f29daa0971
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-13 22:10:05 +0300

    ExpenseForm

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
-function ExpenseForm() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+function ExpenseForm({ addExpense }) {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [amount, setAmount] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (name && description && amount) {
+        const newExpense = {
+          id: Date.now(),
+          name,
+          description,
+          amount: parseFloat(amount),
+        };
+        addExpense(newExpense);
+        setName('');
+        setDescription('');
+        setAmount('');
+           }
+      };
+
+return () {} }
+
+
 
 export default ExpenseForm
